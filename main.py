3b6666a18eee332c4011ff2856cccc5b04662d64
Sosa714 <tsosa592@gmail.com>
2023-10-22 20:43:05 -0600

    Loaded more csv data to be analyzed

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     # Step 1: Exploring the Data
     movies = load_data("movies.csv")
     ratings = load_data("ratings.csv")
+    tags = load_data("tags.csv")
+    genome_scores = load_data("genome-scores.csv")
+    genome_tags = load_data("genome-tags.csv")
 
     print("\nMovies Data:")
     basic_statistics(movies)
@@ -11,5 +14,14 @@ def main():
     print("\nRatings Data:")
     basic_statistics(ratings)
 
+    print("\nTags Data:")
+    basic_statistics(tags)
+
+    print("\nGenome Scores Data:")
+    basic_statistics(genome_scores)
+
+    print("\nGenome Tags Data:")
+    basic_statistics(genome_tags)
+    
 if __name__ == "__main__":
     main()
