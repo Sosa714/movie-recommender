94233041fa4d377dd700b1ca29753b18bc2ea3ea
Sosa714 <tsosa592@gmail.com>
2023-10-29 15:45:45 -0600

    Deleted all of the old file setup

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-"""
-Given a filename, returns the full path to the file located in the data directory.
-"""
-def get_data_path(filename):
-
-    return os.path.join(os.getcwd(), 'data', filename)
